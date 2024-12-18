9745a1f9672f13481084dc7c266bcdd2011b420c
امین پورزارع <apourzare@gmail.com>
2024-12-18 10:56:42 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 sudo apt update
 sudo apt upgrade -y
 
-read -p "Enter DNS 01 :"  DNS1
-read -p "Enter DNS 02 :"  DNS2
+read -p "Enter DNS 01 : "  DNS1
+read -p "Enter DNS 02 : "  DNS2
 
-echo -e "nameserver " + DNS1 + "\nnameserver " + DNS2 > /etc/resolv.conf
+echo -e "nameserver $DNS1 \nnameserver $DNS2" > /etc/resolv.conf
 
-read -p "Enter Username" username
-read -s -p "Enter Password" password
+read -p "Enter Username : " username
+read -s -p "Enter Password : " password
 
 sudo useradd -m $username
 sudo usermod -aG sudo $username
