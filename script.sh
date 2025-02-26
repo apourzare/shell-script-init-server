795f467ce3f1f1e226227a8341994934f3b977c6
امین پورزارع <apourzare@gmail.com>
2025-02-26 13:19:54 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade -y
-
-read -p "Enter DNS 01 : "  DNS1
-read -p "Enter DNS 02 : "  DNS2
-
-echo -e "nameserver $DNS1 \nnameserver $DNS2" > /etc/resolv.conf
-
-read -p "Enter Username : " username
-read -s -p "Enter Password : " password
-
-sudo useradd -m $username
-sudo usermod -aG sudo $username
-echo "$username:$password" | sudo chpasswd
 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
