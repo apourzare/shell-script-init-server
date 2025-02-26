4f52691cd0262f63c60e4088fcc69a3ca3e757bc
امین پورزارع <apourzare@gmail.com>
2025-02-26 13:19:35 +0330

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+sudo apt update
+sudo apt upgrade -y
+
+read -p "Enter DNS 01 : "  DNS1
+read -p "Enter DNS 02 : "  DNS2
+
+echo -e "nameserver $DNS1 \nnameserver $DNS2" > /etc/resolv.conf
+
+read -p "Enter Username : " username
+read -s -p "Enter Password : " password
+
+sudo useradd -m $username
+sudo usermod -aG sudo $username
+echo "$username:$password" | sudo chpasswd
